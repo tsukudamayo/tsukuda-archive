38e358caaac10d03aea443a0d624475267bcc831
tsukuda <v_mayo.tukuda@pactera.com>
2018-12-12 17:42:58 +0900

    add tmv.go 20181212

--- tmv.go
+++ tmv.go
@@ -3,10 +3,15 @@ package main
 import (
     "fmt"
     "os"
+    "path/filepath"
 )
 
 func main() {
-    if err := os.Rename(os.Args[1], os.Args[2]); err != nil {
+    fileName := filepath.Base(os.Args[1])
+    dstDir := os.Args[2]
+    dstFile := filepath.Join(dstDir, fileName)
+    
+    if err := os.Rename(os.Args[1], dstFile); err != nil {
         fmt.Println(err)
     }
 }
